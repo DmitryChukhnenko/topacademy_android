3417c730ee296f19a61eab2f3c8640820e2c6305
DmitryChukhnenko <DmitryChukhnenko@yandex.ru>
2025-05-21 20:11:06 +0300

    Task 4

--- app/src/main/java/com/example/topacademy_android/WeatherActivity.kt
+++ app/src/main/java/com/example/topacademy_android/WeatherActivity.kt
@@ -1,14 +1,8 @@
 package com.example.topacademy_android
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.topacademy_android.databinding.ActivityWeatherBinding
 
 class WeatherActivity : AppCompatActivity() {
